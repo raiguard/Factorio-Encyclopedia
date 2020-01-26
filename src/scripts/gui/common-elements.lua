8a1dfd814ad29dc6dca01ab7475540a9b44349ca
raiguard <caleb.heuer@gmail.com>
2020-01-25 20:29:01 -0700

    Populate item page

--- src/scripts/gui/common-elements.lua
+++ src/scripts/gui/common-elements.lua
@@ -6,35 +6,35 @@ local self = {}
 -- return the most commonly used GUI elements first, then a table of all of them
 
 -- listbox with a label above it, contained in a vertical flow
-function self.listbox_with_label(parent, name, frame_height, label_caption)
-  local flow = parent.add{type='flow', name='fe_'..name..'_flow', direction='vertical'}
-  local label = flow.add{type='label', name='fe_listbox_label', style='fe_listbox_label', caption=label_caption}
-  local listbox_frame = flow.add{type='frame', name='fe_listbox_frame', style='fe_listbox_frame'}
+function self.listbox_with_label(parent, frame_height, label_caption)
+  local flow = parent.add{type='flow', direction='vertical'}
+  local label = flow.add{type='label', style='fe_listbox_label', caption=label_caption}
+  local listbox_frame = flow.add{type='frame', style='fe_listbox_frame'}
   if frame_height then
     listbox_frame.style.height = frame_height
   end
-  local listbox = listbox_frame.add{type='list-box', name='fe_'..name..'_listbox', style='fe_listbox'}
+  local listbox = listbox_frame.add{type='list-box', style='fe_listbox'}
   return listbox, label, {flow=flow, label=label, listbox_frame=listbox_frame, listbox=listbox}
 end
 
 -- the standard "cell content" for info pages - contains a label and a lower flow
-function self.standard_cell(parent, name, label_caption, flow_direction, flow_style)
-  local cell_flow = parent.add{type='flow', name='fe_'..name..'_cell_flow', direction='vertical'}
-  local label = cell_flow.add{type='label', name='fe_cell_title', style='caption_label', caption=label_caption}
+function self.standard_cell(parent, label_caption, flow_direction, flow_style)
+  local cell_flow = parent.add{type='flow', direction='vertical'}
+  local label = cell_flow.add{type='label', style='caption_label', caption=label_caption}
   if not label_caption then label.visible = false end -- hide the label altogether if it doesn't have text
-  local content_flow = cell_flow.add{type='flow', name='fe_cell_content_flow', direction=flow_direction or 'horizontal'}
+  local content_flow = cell_flow.add{type='flow', direction=flow_direction or 'horizontal'}
   return content_flow, {cell_flow=cell_flow, content_flow=content_flow, label=label}
 end
 
 -- standard object info table entry
 function self.info_table_entry(parent, name, value)
   include_pusher = include_pusher or true
-  local flow = parent.add{type='flow', name='fe_cell_'..name, style='fe_vertically_centered_flow', direction='horizontal'}
-  local label = flow.add{type='label', name='fe_label_'..name, style='fe_table_label', caption={'fe-gui.'..name:gsub('_', '-')}}
-  flow.add{type='empty-widget', name='fe_pusher', style='fe_horizontal_pusher'}
+  local flow = parent.add{type='flow', style='fe_vertically_centered_flow', direction='horizontal'}
+  local label = flow.add{type='label', style='fe_table_label', caption={'fe-gui.'..name:gsub('_', '-')}}
+  flow.add{type='empty-widget', style='fe_horizontal_pusher'}
   local value_label
   if value then
-     value_label = flow.add{type='label', name='fe_value_label', style='fe_table_value', caption=value}
+     value_label = flow.add{type='label', style='fe_table_value', caption=value}
   end
   return flow, {flow=flow, label=label, value_label=value_label}
 end
--- src/scripts/gui/info-pages/item.lua
+++ src/scripts/gui/info-pages/item.lua
@@ -5,6 +5,9 @@
 local event = require('lualib/event')
 local gui = require('lualib/gui')
 
+local common_elems = require('scripts/gui/common-elements')
+local common_handlers = require('scripts/gui/common-handlers')
+
 -- locals
 local table_sort = table.sort
 
@@ -24,10 +27,11 @@ local self = {}
 -- GUI DATA
 
 gui.add_handlers('item', {
-  demo_button = {
-    on_gui_click = function(e)
-      event.raise(open_info_gui_event, {player_index=e.player_index, category='item', object_name='assembling-machine-1'})
-    end
+  generic_buttons = {
+    on_gui_click = common_handlers.open_button_content
+  },
+  generic_listboxes = {
+    on_gui_selection_state_changed = common_handlers.open_listbox_content
   }
 })
 
@@ -35,22 +39,76 @@ gui.add_handlers('item', {
 -- OBJECT
 
 function self.create(player, player_table, content_scrollpane, name)
+  local encyclopedia = global.encyclopedia
+  local item_data = encyclopedia.item[name]
+  local dictionary = player_table.dictionary
+
   -- CREATE GUI STRUCTURE
   local gui_data = gui.create(content_scrollpane, 'item', player.index,
     {type='table', style='fe_content_table', column_count=1, children={
-      {type='label', caption='DEBUG'},
-      {type='label', caption='DEBUG'},
-      {type='label', caption='DEBUG'},
-      {type='label', caption='DEBUG'},
-      {type='label', caption='DEBUG'},
-      {type='label', caption='DEBUG'},
-      {type='label', caption='DEBUG'},
-      {type='label', caption='DEBUG'},
-      {type='label', caption='DEBUG'},
-      {type='label', caption='DEBUG'},
-      {type='button', caption='Assembling machine 1', handlers='demo_button'}
+      -- general info table
+      {type='table', style='bordered_table', column_count=1, save_as='general_info_table'},
+      -- recipe usages
+      {type='flow', direction='vertical', save_as='recipe_usages_cell'}
     }}
   )
+
+  -- POPULATE INFO TABLE
+  local generic_buttons = {}
+  local info_table = gui_data.general_info_table
+  for key,action in pairs(general_data) do
+    if action == true then
+      -- common action
+      local data = item_data.prototype[key]
+      if data and (type(data) == 'string' or (type(data) == 'number' and data > 0)) then
+        common_elems.info_table_entry(info_table, key, data)
+      end
+    elseif action == 'place_result' or action == 'place_as_equipment_result' then
+      local place_result = item_data.prototype[key]
+      if place_result then
+        local category = action:find('as_equipment') and 'equipment' or 'entity'
+        local value_flow = common_elems.info_table_entry(info_table, key)
+        generic_buttons[#generic_buttons+1] = value_flow.add{type='button',
+          caption='[img='..category..'/'..place_result.name..']  '..dictionary[category].translations[place_result.name]}
+      end
+    elseif action == 'place_as_tile_result' then
+      local place_result = item_data.prototype[key]
+      if place_result then
+        place_result = place_result.result
+        local value_flow = common_elems.info_table_entry(info_table, key)
+        generic_buttons[#generic_buttons+1] = value_flow.add{type='button',
+          caption='[img=tile/'..place_result.name..']  '..dictionary.tile.translations[place_result.name]}
+      end
+    end
+  end
+
+  -- POPULATE RECIPE USAGES
+  local generic_listboxes = {}
+  if item_data.as_ingredient or item_data.as_product then
+    local cell_flow = common_elems.standard_cell(gui_data.recipe_usages_cell, {'fe-gui.usage-in-recipes'}, 'horizontal')
+    cell_flow.style.horizontal_spacing = 8
+    for _,type in ipairs{'ingredient', 'product'} do
+      local listbox, label = common_elems.listbox_with_label(cell_flow)
+      local as_data = item_data['as_'..type]
+      local add_item = listbox.add_item
+      if as_data then
+        table_sort(as_data)
+        for i=1,#as_data do
+          local recipe_name = as_data[i]
+          add_item('[img=recipe/'..recipe_name..']  '..(dictionary.recipe.translations[recipe_name] or recipe_name))
+        end
+      end
+      label.caption = {'fe-gui.as-'..type, as_data and #as_data or 0}
+      generic_listboxes[#generic_listboxes+1] = listbox
+    end
+  else
+    gui_data.recipe_usages_cell.destroy()
+  end
+
+  -- SET UP GENERIC HANDLERS
+  gui.register_handlers('item', 'generic_buttons', {name='generic_buttons', player_index=player.index, gui_filters=generic_buttons})
+  gui.register_handlers('item', 'generic_listboxes', {name='generic_listboxes', player_index=player.index, gui_filters=generic_listboxes})
+
   return gui_data
 end
 
@@ -67,38 +125,6 @@ function self.create(player, player_table, content_scrollpane, name)
   local item_data = encyclopedia.item[name]
   local dictionary = player_table.dictionary
   local table = content_scrollpane.add{type='table', name='fe_table', style='fe_content_table', column_count=1}
-  --
-  -- GENERAL INFO
-  --
-  do
-    local content_flow = common_elems.standard_cell(table, 'general', nil, 'vertical')
-    local info_table = content_flow.add{type='table', name='fe_info_table', style='bordered_table', column_count=1}
-    for key,action in pairs(general_data) do
-      if action == true then
-        -- common action
-        local data = item_data.prototype[key]
-        if data and (type(data) == 'string' or (type(data) == 'number' and data > 0)) then
-          common_elems.info_table_entry(info_table, key, data)
-        end
-      elseif action == 'place_result' or action == 'place_as_equipment_result' then
-        local place_result = item_data.prototype[key]
-        if place_result then
-          local category = action:find('as_equipment') and 'equipment' or 'entity'
-          local value_flow = common_elems.info_table_entry(info_table, key)
-          elems.buttons[#elems.buttons+1] = value_flow.add{type='button', name='fe_value_button',
-                                            caption='[img='..category..'/'..place_result.name..']  '..dictionary[category].translations[place_result.name]}
-        end
-      elseif action == 'place_as_tile_result' then
-        local place_result = item_data.prototype[key]
-        if place_result then
-          place_result = place_result.result
-          local value_flow = common_elems.info_table_entry(info_table, key)
-          elems.buttons[#elems.buttons+1] = value_flow.add{type='button', name='fe_value_button',
-                                            caption='[img=tile/'..place_result.name..']  '..dictionary.tile.translations[place_result.name]}
-        end
-      end
-    end
-  end
   --
   -- USAGE IN RECIPES
   --
