80642803ea5da951fd17e0deeaaa8bb0c998f5a2
raiguard <caleb.heuer@gmail.com>
2020-01-31 11:45:10 -0700

    Recipe general data table

--- src/scripts/gui/info-pages/recipe.lua
+++ src/scripts/gui/info-pages/recipe.lua
@@ -12,7 +12,11 @@ local common_handlers = require('scripts/gui/common-handlers')
 local table_sort = table.sort
 
 local general_data = {
-  
+  localised_description = true,
+  main_product = 'product',
+  category = true,
+  energy = true,
+  emissions_multiplier = true
 }
 
 -- objects
@@ -36,25 +40,52 @@ gui.add_handlers('item', {
 function self.create(player, player_table, content_scrollpane, name)
   local encyclopedia = global.encyclopedia
   local recipe_data = encyclopedia.recipe[name]
+  local recipe_prototype = recipe_data.prototype
   local dictionary = player_table.dictionary
 
   local generic_buttons = {}
   local generic_listboxes = {}
 
   local gui_data = gui.create(content_scrollpane, 'recipe', player.index,
-    {type='table', style='enc_content_table', column_count=1, save_as='content_table'}
+    {type='table', style='enc_content_table', column_count=1, save_as='content_table', children={
+      {type='table', style='bordered_table', column_count=1, save_as='general_info_table'}
+    }}
   )
 
-  -- SET UP RECIPE INFO
+  -- GENERAL INFO TABLE
+  local info_table = gui_data.general_info_table
+  for key,action in pairs(general_data) do
+    if action == true then
+      -- common action
+      local data = recipe_prototype[key]
+      if data and (type(data) == 'string' or (type(data) == 'number' and data > 0)) then
+        common_elems.info_table_entry(info_table, key, data)
+      end
+    elseif action == 'product' and #recipe_prototype.products > 1 then
+      local product = recipe_prototype[key]
+      if product then
+        local value_flow = common_elems.info_table_entry(info_table, key)
+        generic_buttons[#generic_buttons+1] = value_flow.add{type='button',
+          caption='[img='..product.type..'/'..product.name..']  '..product.amount..'x '..dictionary[product.type].translations[product.name]}
+      end
+    end
+  end
+
+  -- RECIPE INFORMATION
+  local heights = {
+    items = math.min(math.max(#recipe_prototype.ingredients, #recipe_prototype.products) * 28, 168),
+    machines_techs = math.min(math.max(#(recipe_data.made_in or {}), #(recipe_data.unlocked_by or {})) * 28, 168)
+  }
   for row,cell in pairs{items={'ingredients', 'products'}, machines_techs={'made_in', 'unlocked_by'}} do
     local cell_flow = common_elems.standard_cell(gui_data.content_table)
     cell_flow.style.horizontal_spacing = 8
     for _,type in ipairs(cell) do
-      local listbox, label = common_elems.listbox_with_label(cell_flow)
+      local listbox, label, other_elems = common_elems.listbox_with_label(cell_flow)
+      other_elems.listbox_frame.style.height = heights[row]
       local add_item = listbox.add_item
       local objects
       if row == 'items' then
-        objects = recipe_data.prototype[type]
+        objects = recipe_prototype[type]
       else
         objects = recipe_data[type]
       end
